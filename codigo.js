2020a8c994414779d38911db0bfa1f7cfc7f8f1d
jmarcolan <jamarcolan@gmail.com>
2019-06-07 23:31:02 -0700

    Refeito a fatoracao.

--- codigo.js
+++ codigo.js
@@ -5,17 +5,12 @@ let cores = ['#FF5068',
 
 let var_globao_i =0
 let var_globao_cor_tamanho = cores.length
+let var_globao_string_overlay = 'fui2 fazer café'
 
 
-cores.forEach( (nome_cor, i) => {
-    mostra_cores(nome_cor)
-    mostra_coreS_interface(nome_cor)
-    // console.log(i)
-})
 
-
-function controla_array(){
-    mostra_coreS_interface(cores[var_globao_i])
+function logica_software(){
+    mostra_coreS_interface(cores[var_globao_i],var_globao_string_overlay)
     mostra_cores(cores[var_globao_i])
 
     var_globao_i = (var_globao_i + 1)%var_globao_cor_tamanho
@@ -25,17 +20,19 @@ function controla_array(){
 
 
 
-setInterval( controla_array, 1000 );
+setInterval( logica_software , 1000 );
 
 
 
 
-function mostra_coreS_interface(nome){
-// https://gomakethings.com/two-ways-to-set-an-elements-css-with-vanilla-javascript/
+function mostra_coreS_interface(nome,string_overlay= 'fui fazer café'){
+    //baseado no código de gomakething
+    // https://gomakethings.com/two-ways-to-set-an-elements-css-with-vanilla-javascript/
    let  a = document.getElementById('cores')
 
-    a.style.color = 'purple';
-    "purple"
+   a.innerHTML= `<h1 style="font-family:'Courier New', Courier, monospace">${string_overlay} </h1>"`
+
+
     a.style.backgroundColor = nome;
     "#e5e5e5"
     a.style.height = '500px';
@@ -51,8 +48,4 @@ function mostra_cores(cores){
     console.log(cores)
 }
 
-
-function oi_mundo(){
-    console.log("oi mundo ")
-}
 
