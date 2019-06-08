a28b74d0cbce142d1d0d9025509d7b1f56ae840e
jmarcolan <jamarcolan@gmail.com>
2019-06-07 23:53:20 -0700

    Funcionando o overlay com orientação ao objeto

--- codigo.js
+++ codigo.js
@@ -1,51 +1,54 @@
-let cores = ['#FF5068',
+
+class Overlay{
+    constructor(){
+        this.cores = ['#FF5068',
             '#E88546',
             '#FFD659',
             '#4DFF69']
+        this.var_globao_i =0
+        this.var_globao_cor_tamanho = this.cores.length
+        this.var_globao_string_overlay = 'fui2 fazer café'
 
-let var_globao_i =0
-let var_globao_cor_tamanho = cores.length
-let var_globao_string_overlay = 'fui2 fazer café'
+    }
+    
+    logica_software(){
+        console.log(this)
+        this.mostra_coreS_interface(this.cores[this.var_globao_i], this.var_globao_string_overlay)
+        // this.mostra_cores(this.cores[this.var_globao_i])
+    
+        this.var_globao_i = (this.var_globao_i + 1 )% this.var_globao_cor_tamanho
+    
+    
+    }
 
+    mostra_coreS_interface(nome,string_overlay= 'fui fazer café'){
+        //baseado no código de gomakething
+        // https://gomakethings.com/two-ways-to-set-an-elements-css-with-vanilla-javascript/
+        
+       let  a = document.getElementById('cores')
+    
+       a.innerHTML= `<h1 style="font-family:'Courier New', Courier, monospace">${string_overlay} </h1>"`
+    
+    
+        a.style.backgroundColor = nome;
+        "#e5e5e5"
+        a.style.height = '500px';
+        "150px"
+    
+    }
 
-
-function logica_software(){
-    mostra_coreS_interface(cores[var_globao_i],var_globao_string_overlay)
-    mostra_cores(cores[var_globao_i])
-
-    var_globao_i = (var_globao_i + 1)%var_globao_cor_tamanho
 
 
 }
 
 
+over1 = new Overlay()
+let func_nome = (el) => {over1.logica_software()}
 
-setInterval( logica_software , 1000 );
-
-
-
-
-function mostra_coreS_interface(nome,string_overlay= 'fui fazer café'){
-    //baseado no código de gomakething
-    // https://gomakethings.com/two-ways-to-set-an-elements-css-with-vanilla-javascript/
-   let  a = document.getElementById('cores')
-
-   a.innerHTML= `<h1 style="font-family:'Courier New', Courier, monospace">${string_overlay} </h1>"`
-
-
-    a.style.backgroundColor = nome;
-    "#e5e5e5"
-    a.style.height = '500px';
-    "150px"
-
-}
+setInterval(func_nome , 1000 );
 
 
 
 
-
-function mostra_cores(cores){
-    console.log(cores)
-}
 
 
